0f09afc3fc27a632c81a15c8461a45cb1b0cc96e
Saverio Tosi <saverio.tosi@ideato.info>
2019-08-27 23:17:41 +0200

    Improve and test creation

--- src/main/java/tosi/saverio/booking/controller/BookingController.java
+++ src/main/java/tosi/saverio/booking/controller/BookingController.java
@@ -3,6 +3,8 @@ package tosi.saverio.booking.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,10 +20,10 @@ public class BookingController {
     private BookingRepository bookingRepository;
  
     @RequestMapping(value = "/",method= RequestMethod.POST)
-    public String create(@RequestBody Booking reservation) {
+    public ResponseEntity<String> create(@RequestBody Booking reservation) {
         bookingRepository.save(reservation);
 
-        return "Created";
+        return new ResponseEntity<>("Created", HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/",method= RequestMethod.GET)
--- src/test/java/tosi/saverio/booking/controller/BookingControllerTests.java
+++ src/test/java/tosi/saverio/booking/controller/BookingControllerTests.java
@@ -6,8 +6,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+import tosi.saverio.booking.model.entity.Booking;
+
+import java.util.Date;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -19,6 +24,34 @@ public class BookingControllerTests {
 	@Test
 	public void should_return_all_bookings() {
 		this.webClient.get().uri("/").exchange().expectStatus().isOk();
+	}
+
+	@Test
+	public void should_create_booking() {
+		Booking booking = new Booking();
+
+		booking.setCourtId(1L);
+		booking.setFrom(new Date(1522771200000L)); 	// "2018-04-03 18:00"
+		booking.setTo(new Date(1522774800000L));	// "2018-04-03 19:00"
+		booking.setUserId(1L);
+
+		this.webClient
+				.get()
+				.uri("/")
+				.exchange().expectStatus().isEqualTo(HttpStatus.OK)
+				.expectBodyList(Booking.class).hasSize(0);
+
+		this.webClient
+				.post()
+				.uri("/")
+				.body(Mono.just(booking), Booking.class)
+				.exchange().expectStatus().isEqualTo(HttpStatus.CREATED);
+
+		this.webClient
+				.get()
+				.uri("/")
+				.exchange().expectStatus().isEqualTo(HttpStatus.OK)
+				.expectBodyList(Booking.class).hasSize(1);
 	}
 
 }

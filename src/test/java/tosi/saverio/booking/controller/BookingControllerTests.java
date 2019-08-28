7945ba7b26a94bda1bb062e632202d878f0d5a6f
Saverio Tosi <saverio.tosi@ideato.info>
2019-08-28 18:45:12 +0200

    Adds test for overlapping booking

--- src/test/java/tosi/saverio/booking/controller/BookingControllerTests.java
+++ src/test/java/tosi/saverio/booking/controller/BookingControllerTests.java
@@ -12,11 +12,13 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 import tosi.saverio.booking.model.entity.Booking;
 
+import java.text.*;
 import java.util.Date;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class BookingControllerTests {
+	private DateFormat df = new SimpleDateFormat("YYYY-MM-DD hh:mm");
 
 	@Autowired
 	private WebTestClient webClient;
@@ -27,12 +29,12 @@ public class BookingControllerTests {
 	}
 
 	@Test
-	public void should_create_booking() {
+	public void the_slot_should_be_available() throws ParseException {
 		Booking booking = new Booking();
 
 		booking.setCourtId(1L);
-		booking.setFrom(new Date(1522771200000L)); 	// "2018-04-03 18:00"
-		booking.setTo(new Date(1522774800000L));	// "2018-04-03 19:00"
+		booking.setFrom(df.parse("2018-04-04 18:00"));
+		booking.setTo(df.parse("2018-04-04 19:00"));
 		booking.setUserId(1L);
 
 		this.webClient
@@ -52,6 +54,58 @@ public class BookingControllerTests {
 				.uri("/")
 				.exchange().expectStatus().isEqualTo(HttpStatus.OK)
 				.expectBodyList(Booking.class).hasSize(1);
+	}
+
+	@Test
+	public void the_slot_should_be_unavailable() throws ParseException {
+		Booking booking1 = new Booking();
+		booking1.setCourtId(1L);
+		booking1.setFrom(df.parse("2018-04-03 16:00"));
+		booking1.setTo(df.parse("2018-04-03 18:00"));
+		booking1.setUserId(1L);
+
+		Booking booking2 = new Booking();
+		booking2.setCourtId(1L);
+		booking2.setFrom(df.parse("2018-04-03 18:00"));
+		booking2.setTo(df.parse("2018-04-03 19:00"));
+		booking2.setUserId(1L);
+
+		Booking booking3 = new Booking();
+		booking3.setCourtId(1L);
+		booking3.setFrom(df.parse("2018-04-03 19:00"));
+		booking3.setTo(df.parse("2018-04-03 21:00"));
+		booking3.setUserId(1L);
+
+		Booking booking4 = new Booking();
+		booking4.setCourtId(1L);
+		booking4.setFrom(df.parse("2018-04-03 17:00"));
+		booking4.setTo(df.parse("2018-04-03 20:00"));
+		booking4.setUserId(1L);
+
+		this.webClient
+				.post()
+				.uri("/")
+				.body(Mono.just(booking1), Booking.class)
+				.exchange().expectStatus().isEqualTo(HttpStatus.CREATED);
+
+		this.webClient
+				.post()
+				.uri("/")
+				.body(Mono.just(booking2), Booking.class)
+				.exchange().expectStatus().isEqualTo(HttpStatus.CREATED);
+
+		this.webClient
+				.post()
+				.uri("/")
+				.body(Mono.just(booking3), Booking.class)
+				.exchange().expectStatus().isEqualTo(HttpStatus.CREATED);
+
+		this.webClient
+				.post()
+				.uri("/")
+				.body(Mono.just(booking4), Booking.class)
+				.exchange().expectStatus().isEqualTo(HttpStatus.BAD_REQUEST);
+
 	}
 
 }
